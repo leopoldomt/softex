84471d414464a2cc13cbf7f27afcea1e1707e036
Leopoldo Teixeira <lmt@cin.ufpe.br>
2023-08-22 14:33:02 -0300

    adicionando sort

---
+++ app/src/main/java/br/ufpe/cin/residencia/aula/busca/BubbleSort.java
@@ -0,0 +1,8 @@
+package br.ufpe.cin.residencia.aula.busca;
+
+public class BubbleSort implements Sort {
+    @Override
+    public void sort(int[] elementos) {
+        
+    }
+}
---
+++ app/src/main/java/br/ufpe/cin/residencia/aula/busca/Sort.java
@@ -0,0 +1,7 @@
+package br.ufpe.cin.residencia.aula.busca;
+
+public interface Sort {
+    //este método recebe um array de inteiros, e reordena este array em ordem crescente
+    void sort(int[] elementos);
+}
+
