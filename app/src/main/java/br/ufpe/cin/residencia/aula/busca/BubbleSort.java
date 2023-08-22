0702d34cb4c076391d58690ff5a638a345d15e79
Leopoldo Teixeira <lmt@cin.ufpe.br>
2023-08-22 14:33:21 -0300

    adicionando bubblesort test

--- app/src/main/java/br/ufpe/cin/residencia/aula/busca/BubbleSort.java
+++ app/src/main/java/br/ufpe/cin/residencia/aula/busca/BubbleSort.java
@@ -3,6 +3,6 @@ package br.ufpe.cin.residencia.aula.busca;
 public class BubbleSort implements Sort {
     @Override
     public void sort(int[] elementos) {
-        
+
     }
 }
---
+++ app/src/test/java/br/ufpe/cin/residencia/aula/busca/BubbleSortTest.java
@@ -0,0 +1,12 @@
+package br.ufpe.cin.residencia.aula.busca;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+
+    @Test
+    void sort() {
+    }
+}
